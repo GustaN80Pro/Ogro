011277868f3f88941160d236719a533d622f5464
Gustavo Maia <gustavomaian80@gmail.com>
2023-10-28 12:07:18 -0300

    Atualização do jogo do ogro

--- jogo_do_ogro.py
+++ jogo_do_ogro.py
@@ -9,19 +9,19 @@ def animacao_personagem():
     jogador_index += 0.11
     if jogador_index >= len(jogador_parado_superficies):
         jogador_index = 0
+    
     # Desenha o jogador na tela
     tela.blit(jogador_parado_superficies[int(jogador_index)], jogador_retangulo.topleft)
 
-# Avança para o proximo frame
-    jogador_index += 0.11
-    if jogador_index > len(jogador_superficies) - 1:
-        jogador_index = 0
+def animacao_ataque1():
+    global jogador_index, ataque1_frame, jogador_atacando
 
-    if direcao_personagem == 1:
-        personagem = pygame.transform.flip(jogador_superficies[int(jogador_index)], True, False)
+    if ataque1_frame < len(jogador_ataque2):
+        tela.blit(jogador_ataque2[ataque1_frame], jogador_retangulo)
+        ataque1_frame += 1
     else:
-        personagem = jogador_superficies[int(jogador_index)]
-
+        ataque1_frame = 0
+        jogador_atacando = False
 
 def animacao_ogro():
     global ogro_index
@@ -41,7 +41,8 @@ tela = pygame.display.set_mode(tamanho)
 
 jogador_index = 0
 jogador_parado_superficies = []
-movimento = 0
+jogador_atacando = False
+jogador_ataque2 = [] 
 
 # Carrega o jogador parado
 for imagem in range(1, 7):
@@ -49,10 +50,11 @@ for imagem in range(1, 7):
     img = pygame.transform.scale(img, (500, 500))
     jogador_parado_superficies.append(img)
 
-for image in range(1, 8):
-    img = pygame.image.load(f'Protagonista/PERSONAGEM1/ANDANDO/Title{image}.png').convert_alpha()
+# Carrega as imagens de ataque
+for image in range(1, 7):
+    img = pygame.image.load(f'Protagonista/PERSONAGEM1/ATAQUE2/tile{image}.png').convert_alpha()
     img = pygame.transform.scale(img, (500, 500))
-    movimento.append()
+    jogador_ataque2.append(img)
 
 # Inicializa o retângulo do jogador
 jogador_retangulo = jogador_parado_superficies[jogador_index].get_rect(center=(100, 300))
@@ -69,12 +71,6 @@ for imagem in range(1, 5):
 # Inicializa o retângulo do ogro
 ogro_retangulo = ogro_parado_superficies[ogro_index].get_rect(center=(100, 315))
 
-# Carrega o jogador andandoda
-for imagem in range(1, 7):
-    img = pygame.image.load(f'Protagonista/PERSONAGEM1/PARADO/tile{imagem}.png').convert_alpha()
-    img = pygame.transform.scale(img, (500, 500))
-    jogador_parado_superficies.append(img)
-
 pygame.display.set_caption("Mata Ogro")
 
 relogio = pygame.time.Clock()
@@ -87,12 +83,21 @@ movimento_personagem = 5
 # Transforma o tamanho da imagem de fundo
 principal = pygame.transform.scale(principal, tamanho)
 
+ataque1_frame = 0 
+
 # Loop principal do jogo
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    # Teclas para atacar
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        jogador_atacando = True
+        jogador_index = 0
+        ataque1_frame = 0
             
     # Atualiza a posição do jogador com base nas teclas pressionadas
     keys = pygame.key.get_pressed()
@@ -105,7 +110,11 @@ while True:
     # Desenha o fundo na tela
     tela.blit(principal, (0, 0))
 
-    animacao_personagem()
+    if jogador_atacando:
+        animacao_ataque1()
+    else:
+        animacao_personagem()
+
     animacao_ogro()
 
     pygame.display.update()
